4103307d015e9914fb4d71f4c87a60edfd6618e0
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2022-10-08 12:13:14 -0700

    deploy gcsw frontend to heroku

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Home from "./components/home.component";
+import UserList from "./components/user-list.component"
 import CreateUser from "./components/create-user.component"; 
 import NavbarComponent from "./components/navbar.component";
 
 
 function App() {
   return (
-    <div className="App">
     <Router>
+      <div className="container">
       <NavbarComponent />
+      <br />
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/user" element={<CreateUser />}  />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/create" element={<CreateUser />}  />
       </Routes>
+      </div>
     </Router>
-    </div>
   );
 }
 
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -6,16 +6,16 @@ import Container from 'react-bootstrap/Container';
 export default class NavbarComponent extends Component {
     render () {
         return (
-            
             <Navbar bg="dark" variant="dark">
                 <Container>
-                <Navbar.Brand href="/">Greenfield Community Science Workshop</Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                </Nav>
+                    <Navbar.Brand href="/">Greenfield Community Science Workshop</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/users">Users</Nav.Link>
+                        <Nav.Link href="/create">Create User</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
-            
         );
     };
 }
