61330c2c72b94fa391234433502827a7d67747dc
Geoffrey Vasaya <gvasaya@csumb.edu>
2023-03-18 04:35:22 -0700

    print button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useReactToPrint } from 'react-to-print';
+import { useRef } from "react";
 
 import ParticipantSignin from "./components/participant-signin.component"
 import Loading from "./components/loading.component";
@@ -11,30 +11,36 @@ import ProtectedRoute from "./auth/protected-route";
 import ParticipantList from "./components/participant-list.component"
 import EditParticipant from "./components/edit-participant.component";
 import NavBar from "./components/navbar.component";
-import PrintPage from "./components/print_pg.component";
-
+import ReactToPrint from "react-to-print";
 
 const App = () => {
   const { isLoading } = useAuth0();
-
+  const componentRef = useRef()
+  const handlePrint = () => {
+    window.print()
+  }
   if (isLoading) {
     return <Loading />;
   }
-  
   return (
-      <div id="app" className="d-flex flex-column h-100">
+      <div id="app" className="d-flex flex-column h-100" handlePrint={handlePrint}>
         
         <NavBar />
+        <ReactToPrint  
+        trigger={() => <button> Print </button>}
+        content = {() => componentRef.content}/>
+        <div ref = {componentRef}>
        
-        <div className="container flex-grow-1">
-        
-          <Routes>
-            <Route path="/" exact element={<ParticipantSignin />} />
-            <Route element={<ProtectedRoute />} >
-                <Route path="/participants" element={<ParticipantList />} />
-                <Route path="/edit/:id" element={<EditParticipant />} />
-            </Route>
-          </Routes>
+          <div className="container flex-grow-1">
+          
+            <Routes>
+              <Route path="/" exact element={<ParticipantSignin />} />
+              <Route element={<ProtectedRoute />} >
+                  <Route path="/participants" element={<ParticipantList />} />
+                  <Route path="/edit/:id" element={<EditParticipant />} />
+              </Route>
+            </Routes>
+          </div>
         </div>
         <Footer />
       </div>
