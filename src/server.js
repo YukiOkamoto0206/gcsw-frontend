c1ae574453dd7549b8d4b2637ff66554ddbff0a1
Ulysses Zamora <leezam01@gmail.com>
2023-04-02 23:59:50 -0700

    fixed routes

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 4000;
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve("public", 'index.html'));
     })
 }
 
